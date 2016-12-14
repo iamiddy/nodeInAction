4ea88b1716c3ca29d4a2daaef7bc6bd52730c821
Iddy Magohe <iddyiam@gmail.com>
2016-12-13 21:20:32 -0500

    async programming

--- hallo-world/app/index.js
+++ hallo-world/app/index.js
@@ -1,5 +1,6 @@
 const calc = require('./calc');
 
+
 const numbersToAdd = [
     3,
     4,
@@ -8,4 +9,18 @@ const numbersToAdd = [
 ];
 
 const result = calc.sum(numbersToAdd);
-console.log(`The result is : ${result}`)
+console.log(`The result is : ${result}`);
+//console.log(process.cwd());
+
+const fs = require('fs');
+console.log('start reading file ...');
+fs.readFile('file.md','utf-8', function(err, content){
+    if (err) {
+         console.log('error happened during reading the  file');
+         return console.log(err);
+    }
+
+    console.log(conent);
+})
+
+console.log('end of the file');
