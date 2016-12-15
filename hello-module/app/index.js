a4e0d5586d6343d2556bd7d3de93a5f0c07ec458
rmi865 <iddy.magohe@capitalone.com>
2016-12-15 15:39:09 -0500

    routing with both query and path parms

--- hello-module/app/index.js
+++ hello-module/app/index.js
@@ -1,16 +1,38 @@
 // app/index.js
 var Currency = require('./currency');
+var express = require('express')
+var router = express.Router()
+var url = require('url')
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
 
 var changeToday = 0.91;
 
 var currency = new Currency(changeToday);
 
-var change = currency.canadianToUs(100);
+// define the home page route :userId/books/:bookId
+router.get('/:from/:to/:amount', function (req, res) {
+   var query = url.parse(req.url,true).query;
+    var from = req.params.from;
+    var to = req.params.to;
+    var amount = req.params.amount;
+     console.log(query)
+     if(query.size) console.log(query.size);
+    var change = from == 'CN'? currency.canadianToUs(amount): currency.canadianToUs(amount);
+  res.json({
+      "message" : `exchange ${amount} from ${from} to ${to}`,
+      "change" : change
 
-console.log(`100 Canadian Dollar equal to this amount of USD ${change} `);
+  })
+})
+// define the about route
+router.get('/', function (req, res) {
+    console.log(url.parse(req.url,true).query)
+  res.send('About exchange rates');
+})
 
-
-
-
-
-
+module.exports = router
--- hello-module/index.js
+++ hello-module/index.js
@@ -1,2 +1,30 @@
 // index.js
- require('./app/index');
+ var exchange = require('./app/index');
+const express = require('express') // un-opinionsted web framework
+const bodyParser = require('body-parser'); // json response body
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json()); // json responses.
+
+const port = 3000;
+
+
+app.use('/exchanges', exchange);
+
+//error handling
+app.use((err,request,response, next) => {
+    console.log(err)
+    response.status(500).json({
+        error: 'something broke'
+    })
+})
+
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
+})
