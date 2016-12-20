ba964248fcf5f0c612dcdab4881f162ae21495a4
Iddy Magohe <iddyiam@gmail.com>
2016-12-19 20:53:13 -0500

    users route

--- api-auth-jwt/app/index.js
+++ api-auth-jwt/app/index.js
@@ -2,13 +2,21 @@
 var express = require('express');
 var apiRouter = express.Router();
 var url = require('url');
-
+var User = require('../models/user');
 
 // define the about route
 apiRouter.get('/', function (req, res) {
     //console.log(url.parse(req.url,true).query)
-    res.status(200).send('APIsssss');
+    res.status(200).json({
+        message: 'Welcome to the coolest API on earth!'
+    });
 });
 
+apiRouter.get('/users', function(req, res){
+    User.find({}, function(err, users){
+        res.json(users);
+    });
+
+});
 
 module.exports = apiRouter
