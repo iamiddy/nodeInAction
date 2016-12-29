23e44098e07736eb9959be6c42ae0e8b94dd27a6
Iddy Magohe <iddyiam@gmail.com>
2016-12-29 17:12:42 -0500

    using used njwt

--- api-auth-jwt/app/index.js
+++ api-auth-jwt/app/index.js
@@ -3,10 +3,13 @@ var express = require('express');
 var apiRouter = express.Router();
 var url = require('url');
 var User = require('../models/user');
-var jwt = require('jsonwebtoken') // used to create, sign and verify tokens
+var nJwt = require('njwt'); // used to create, sign and verify tokens
+var secureRandom = require('secure-random');
 var config = require('../config'); // get config file
 
-
+//
+//var signingKey = secureRandom(256, {type: 'Buffer'}); // Create a highly random byte array of 256 bytes
+//var base64SigningKey = signingKey.toString('base64');
 
 // define the about route
 apiRouter.get('/', function (req, res) {
@@ -38,10 +41,19 @@ apiRouter.post('/authenticate', function(req,res){
                 });
             }else {
                 // if user is found and password is right
-                // create a token
-                var token = jwt.sign(user, config.secret, {
-                    expiresIn: 1440 // expires in 24hrs
-                });
+                 var claims = {
+                    iss: "http://myapp.com/",  // The URL of your service
+                    sub: user.username,    // The UID of the user in your system
+                    scope: "self, admins" // change these accordingly
+                    }
+
+                 // create a token
+                    var jwt = nJwt.create(claims, config.secret);
+                    jwt.setExpiration(new Date().getTime() + (60*60*1000)); // One hour from now
+                    //console.log(jwt);
+
+                var token = jwt.compact();
+                    //console.log(token);
 
                 // return the information including token as JSON
                 res.json({
--- api-auth-jwt/config.js
+++ api-auth-jwt/config.js
@@ -1,6 +1,6 @@
 // config.exports
 module.exports = {
-    'secret': 'someverysecretkey',
+    'secret': 'Ofl7vjeUddBliwDeEcR28v4eVOQFhdtXjCQh3ll2wsG5Set7QkL9yIfolt7FPvFQfrxNDV0LMoo4BvxuEtMLFNoYbbdIqiz1NIlmFaYvEx4MX8IwFoAnW78l7NJnKrv22/C8RX95g3XEPbinQbDKrKRrbgvx53lnrVX6XnuJ5SUbXjxF4wQLc+7w3yl6q3edicgNOIY5A2WasZ2eVekgu22joAHgDoMh3UjWU9nJurzIpC/P7KviQVbMrzPDI5Xe9SxIlR7Ey88RX/js9hmZXqfKw9KAmcMsRfc3EqIfNuUJIPNHUo17gaEYaIryPj/vN0uhlrJZTUOAf48n4956OQ==',
     'databaseUrl': 'mongodb://nodeuser:nodepwd@localhost:27017/node-users'
 };
 
